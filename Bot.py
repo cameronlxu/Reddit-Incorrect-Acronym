775be7e14e54362d4544f04227ef7a0ab6b8994e
Cameron Lau <cameron1998@gmail.com>
2020-11-22 17:07:21 -0800

    readjusted file structure, implemeted comment ID encryption/decryption, adjusted Fernet key generation/loading

--- Bot.py
+++ Bot.py
@@ -1,16 +1,18 @@
 import praw
 import json
 import random
+from cryptography.fernet import Fernet
 import Settings as botInfo
 
+
 # Open json files and assign them to variables
-with open('./words_dictionary.json') as dictionary:
+with open('./Data/words_dictionary.json') as dictionary:
     words = json.load(dictionary)
 
-with open('./custom_dictionary.json') as cust_dictionary:
+with open('./Data/custom_dictionary.json') as cust_dictionary:
     custom_words = json.load(cust_dictionary)
 
-with open('./posts_replied_to.json') as posts_replied_to:
+with open('./Data/posts_replied_to.json') as posts_replied_to:
     posts_replied = json.load(posts_replied_to)
 
 # Create reddit instance
@@ -33,14 +35,14 @@ def main():
         # comment.body is one string, use .split() to separate each word into an array
         viewComment = comment.body.split()
 
-        # Ensure bot doesn't deal with itself
+        # Ensure bot doesn't reply to itself
         if comment.author.name == "incorrectacronymbot":
             print("This comment is mine! ID:", comment)
             continue
 
-        # Check if we have already replied to a comment by checking the comment ID
-        if str(comment) in posts_replied:
-            print("I have already replied to this post! ID:", comment)
+        # Check if we have already replied to a comment
+        found = check_commentExists(comment)
+        if found == True:
             continue
         
         # Check if the comment is using command !Suggest
@@ -57,7 +59,48 @@ def main():
         # Write to post_replied_to since we created a new comment
         write_posts_replied_to(comment)
 
-def Suggest(comment, viewComment):
+
+def generate_secret_key() -> None:
+    """
+        Generate a new Fernet key, and write it to ./secret.key
+    """
+    key = Fernet.generate_key()
+    with open("./secret.key", "wb") as key_file:
+        key_file.write(key)
+
+def load_secret_key() -> bytes:
+    """
+        Load the BYTES type key in secret.key.
+        If ./secret.key is empty, call generate_secret_key() to generate a new Fernet key. 
+    """
+    key = open("./secret.key", "rb").read()
+
+    # If the Fernet key doesn't exist, create a Fernet key
+    if key == b'':
+        generate_secret_key()
+
+    return open("./secret.key", "rb").read()
+
+
+def check_commentExists(comment) -> bool:
+    """
+        Decrypt each key in posts_replied_to.json, then compare it to the input comment.
+        If equal, return True. Else return False. 
+    """
+    secret_key = load_secret_key()
+    f = Fernet(secret_key)
+    for key in posts_replied.keys():
+        # Decrypt function only takes in type BYTE, thus need to encode the key
+        decrypted_key = f.decrypt(key.encode())
+
+        # To compare the key and comment, cast both to strings
+        if str(decrypted_key.decode()) == str(comment):
+            print("I have already replied to this post! ID:", comment)
+            return True
+    return False
+
+
+def Suggest(comment, viewComment) -> None:
     """
         Suggest function:
             - add acronym & definiton to custom_dictionary
@@ -76,11 +119,11 @@ def Suggest(comment, viewComment):
     custom_words[key] = value   # Set key & value in custom_words
 
     # Cast comment ID into string and add it to post_replied, using dummy value 1
-    posts_replied[str(comment)] = 1
+    write_posts_replied_to(comment)
 
     # Serializing and writing to custom_dictionary.json  
     json_custom_words = json.dumps(custom_words, indent = 4) 
-    with open("custom_dictionary.json", "w") as outfile: 
+    with open("./Data/custom_dictionary.json", "w") as outfile: 
         outfile.write(json_custom_words)
     
     # Format and send reply comment
@@ -91,7 +134,14 @@ def Suggest(comment, viewComment):
     comment.reply(reply)
     print("Added [" + key + "] [" + value + "] to custom dictionary!")
 
-def createDefiniton(full_definition, comment, viewComment):
+
+def createDefiniton(full_definition, comment, viewComment) -> None:
+    """
+        Create Definition function:
+            - check if word is in custom dictionary
+            - check if word is not in words dictionary
+            - format & send reply comment
+    """
     for word in viewComment:
         lowered_word = word.lower()
         # Prioritize words in custom_dictionary.json
@@ -128,13 +178,27 @@ def createDefiniton(full_definition, comment, viewComment):
         comment.reply(reply)
         print("posted comment ID: {}!".format(comment))
 
-def write_posts_replied_to(comment):
-    # Cast comment ID into string and add it to post_replied, using dummy value 1
-    posts_replied[str(comment)] = 1
+
+def write_posts_replied_to(comment) -> None:
+    """
+        Replied-To function:
+            - Check if comment ID is in replied-to
+            - If not then add comment ID to json
+    """
+    # Load Fernet Secret Key
+    secret_key = load_secret_key()
+    f = Fernet(secret_key)
+
+    # Encrypt function parameter has to be of type BYTE, so encode the comment to be of type BYTE
+    key = str(comment).encode()
+    token = f.encrypt(key)
+
+    # A dictionary cannot take BYTE type as key, so cast to string by decoding. Set dummy value of 1
+    posts_replied[token.decode()] = 1
 
     # Serializing and writing to posts_replied_to.json
     json_posts_replied = json.dumps(posts_replied, indent = 4) 
-    with open("posts_replied_to.json", "w") as outfiles:
+    with open("./Data/posts_replied_to.json", "w") as outfiles:
         outfiles.write(json_posts_replied)
 
 
